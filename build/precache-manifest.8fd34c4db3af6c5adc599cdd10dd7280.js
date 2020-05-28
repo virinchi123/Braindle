4b8dc8928e62c8d30fe4576c88f35568a3b63433
virinchi123 <raovirinchi123@gmail.com>
2020-05-28 10:14:04 +0530

    added build files

---
+++ build/precache-manifest.8fd34c4db3af6c5adc599cdd10dd7280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd99b213eb6237bee18e3ab000b3811",
+    "url": "/index.html"
+  },
+  {
+    "revision": "950dc4a2d7a25fe49a3c",
+    "url": "/static/css/main.d9329d7d.chunk.css"
+  },
+  {
+    "revision": "849ea1133c3373b98e94",
+    "url": "/static/js/2.bf83892a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.bf83892a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "950dc4a2d7a25fe49a3c",
+    "url": "/static/js/main.65eb2074.chunk.js"
+  },
+  {
+    "revision": "daafab4e033226171d4e",
+    "url": "/static/js/runtime-main.ab669cec.js"
+  }
+]);
